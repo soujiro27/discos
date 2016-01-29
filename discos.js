0d5f5dc1c8ddde88e572080573fff8a7e2044ccc
soujiro27 <jc.raus27@gmail.com>
2016-01-29 11:36:37 -0600

    ya finalizado chevere

--- discos.js
+++ discos.js
@@ -9,10 +9,10 @@ function inicia()
 function compra()
 {
 var nodo=$(this).parent().children().first().next().text();
-
 $('aside').append('<li>'+nodo+'</li>');
 var precio=$(this).parent().children().first().next().next().next().text();
 precio=parseInt(precio);
 total=total+precio;
 $('div').html(total);
+$(this).parent().hide('slow');
 }
